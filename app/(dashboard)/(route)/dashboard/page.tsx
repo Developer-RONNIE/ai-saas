8eeec009055f0d11a67ef03cab2ee13ced1c0d30
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-06-14 11:40:08 +0530

    Environment & Folder Set Up

---
+++ app/(dashboard)/(route)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () =>  {
+  return (
+    <div>Dashboard Page (Protected) </div>
+          
+  )
+}
+
+
+export default DashboardPage;
