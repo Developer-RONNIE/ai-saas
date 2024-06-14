6576755b9e2c10b38ccb07c11e5f392b7b9910ea
Developer-RONNIE <ronnieworkspace@gmail.com>
2024-06-15 00:27:41 +0530

    responsive sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
 
 const DashboardLayout = ({
     children
@@ -8,9 +9,7 @@ const DashboardLayout = ({
     return ( 
         <div className="h-full relative">
             <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
-                <div>
-                    Hello Sidebar
-                </div>
+                <Sidebar />
             </div>
             <main className="md:pl-72">
                 <Navbar />
